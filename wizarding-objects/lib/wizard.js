5565d66e2ec35c3a118af85c5f55b8e215e20d92
Michael Alex Lynch <mlynch5187@gmail.com>
2021-07-12 09:37:24 -0600

    Wizards have a name

--- wizarding-objects/lib/wizard.js
+++ wizarding-objects/lib/wizard.js
@@ -1,5 +1,11 @@
-class Wizard  {
+var Pet = require('./pet');
 
+class Wizard  {
+  constructor(obj) {
+    obj = obj || {};
+    this.name = `${obj.name}`;
+    this.pet = [];
+  }
 }
 
 module.exports = Wizard;
